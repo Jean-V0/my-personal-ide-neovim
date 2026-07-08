510aca13c278ab5fe1353d0dd9af60164eaade41
Jean-V0 <jean.baguetinha@gmail.com>
2026-07-08 18:43:33 -0300

    autopair para colchetes e tals e barbar para transitar entre arquivos

---
+++ lua/jean/lazy/barbar.lua
@@ -0,0 +1,24 @@
+vim.keymap.set("n", "<S-h>", "<Cmd>BufferPrevious<CR>")
+vim.keymap.set("n", "<S-l>", "<Cmd>BufferNext<CR>")
+
+for i = 1, 9 do
+  vim.keymap.set("n", "<A-" .. i .. ">", "<Cmd>BufferGoto " .. i .. "<CR>")
+end
+
+return {
+  {
+    'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    },
+    init = function() vim.g.barbar_auto_setup = false end,
+    opts = {
+      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+      animation = true,
+      -- insert_at_start = true,
+      -- …etc.
+    },
+    version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  },
+}
